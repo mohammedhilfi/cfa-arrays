4cbc1aee577266b2c8514ad6874e2f19fe6274db
Kayla Golder <golderkayla@gmail.com>
2021-10-14 19:42:09 -0400

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,19 @@
+//1 create an array of strings (has to be of at least length 5)
+
+//2 print out the second element in the array
+
+//3 print out the length of the array
+
+//4 iterate over the array and print out each element
+
+//5 create a function that takes in a string and in the function iterate over each element in the array and for each element check if its equal to the parameter
+  // if it is then return that string
+  //else continue
+  
+ //6 remove the last element from the array and then console.log the array
+ 
+ //7 remove the first element from the array and then console.log the array
+ 
+ //8 add a string to the end of the array and then console.log the array
+ 
+ // add a string to the beginning of the array and then console.log the array
